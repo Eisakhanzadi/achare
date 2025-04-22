b66e4b545b1dd9ecdf525cb43abf84ef48b6a64f
Eisa khanzadi <esakhanzadi@gmail.com>
2025-04-23 01:09:26 +0330

    wip in first page change container

--- src/views/NewAddressView.vue
+++ src/views/NewAddressView.vue
@@ -1,39 +1,41 @@
 <template>
-  <section class="form-container">
-    <div>
-      <h3>ثبت آدرس</h3>
-    </div>
-    <div class=" card  h-100 border-0  px-4 pb-4 pt-2">
-      <div class="card-header border-0 bg-transparent">
-        <h4>لطفا مشخصات و آدرس خود را وارد کنید </h4>
+  <section class="">
+    <div class="form-container">
+      <div>
+        <h3>ثبت آدرس</h3>
       </div>
-      <div class="card-body row d-flex align-items-center">
-        <form-input @send-data="data['name'] = $event" class="col col-lg-6 col-xl-4" id="name"
-                    title="نام" placeholder="مثال: عیسی" type="text" min-length="3"
-                    :validation="validationError.name"
-                    :require-input="false" error="نام باید حداقل دارای 3 کاراکتر باشد" />
-        <form-input @send-data="data['lastName'] = $event" class="col col-lg-6 col-xl-4"
-                    id="last-name"
-                    title="نام خانوادگی" placeholder="مثال: رضایی" type="text" min-length="3"
-                    :validation="validationError.lastName" :require-input="false"
-                    error="نام خانوادگی باید حداقل دارای 3 کاراکتر باشد" />
-        <form-input @send-data="data['phoneNumber'] = $event" class="col col-lg-6 col-xl-4"
-                    id="phone-number" title="شماره تلفن همراه" placeholder="مثال: 09121234567"
-                    type="text" min-length="11" :validation="validationError.phoneNumber"
-                    :require-input="false"
-                    error="شماره وارد شده صحیح نمیباشد" />
-        <form-input @send-data="data['telephone'] = $event" class="col col-lg-6 col-xl-4"
-                    id="telephone"
-                    title="شماره تلفن ثابت" placeholder="مثال: 0211234567" type="text"
-                    min-length="11"
-                    :validation="validationError.telephone" :require-input="false"
-                    error="شماره وارد شده صحیح نمیباشد" />
-        <form-input @send-data="data['address'] = $event" class="col col-lg-12 col-xl-8"
-                    id="address"
-                    title="آدرس" placeholder=" " type="text" min-length="10"
-                    :validation="validationError.address"
-                    :require-input="false" error="آدرس باید حداقل 11 کارکتر باشد  " />
-        <div class="col"> {{ data }}</div>
+      <div class=" card  h-100 border-0  px-4 pb-4 pt-2">
+        <div class="card-header border-0 bg-transparent">
+          <h4>لطفا مشخصات و آدرس خود را وارد کنید </h4>
+        </div>
+        <div class="card-body row d-flex align-items-center">
+          <form-input @send-data="data['name'] = $event" class="col col-lg-6 col-xl-4" id="name"
+                      title="نام" placeholder="مثال: عیسی" type="text" min-length="3"
+                      :validation="validationError.name"
+                      :require-input="false" error="نام باید حداقل دارای 3 کاراکتر باشد" />
+          <form-input @send-data="data['lastName'] = $event" class="col col-lg-6 col-xl-4"
+                      id="last-name"
+                      title="نام خانوادگی" placeholder="مثال: رضایی" type="text" min-length="3"
+                      :validation="validationError.lastName" :require-input="false"
+                      error="نام خانوادگی باید حداقل دارای 3 کاراکتر باشد" />
+          <form-input @send-data="data['phoneNumber'] = $event" class="col col-lg-6 col-xl-4"
+                      id="phone-number" title="شماره تلفن همراه" placeholder="مثال: 09121234567"
+                      type="text" min-length="11" :validation="validationError.phoneNumber"
+                      :require-input="false"
+                      error="شماره وارد شده صحیح نمیباشد" />
+          <form-input @send-data="data['telephone'] = $event" class="col col-lg-6 col-xl-4"
+                      id="telephone"
+                      title="شماره تلفن ثابت" placeholder="مثال: 0211234567" type="text"
+                      min-length="11"
+                      :validation="validationError.telephone" :require-input="false"
+                      error="شماره وارد شده صحیح نمیباشد" />
+          <form-input @send-data="data['address'] = $event" class="col col-lg-12 col-xl-8"
+                      id="address"
+                      title="آدرس" placeholder=" " type="text" min-length="10"
+                      :validation="validationError.address"
+                      :require-input="false" error="آدرس باید حداقل 11 کارکتر باشد  " />
+          <div class="col"> {{ data }}</div>
+        </div>
       </div>
     </div>
 
