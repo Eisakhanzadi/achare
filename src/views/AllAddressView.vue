d8adf9d0bb6b10a3919b4068369cc470d8004cb9
Eisakhanzadi <esakhanzadi@gmail.com>
2025-04-23 12:11:50 +0330

    add Title for pages

--- src/views/AllAddressView.vue
+++ src/views/AllAddressView.vue
@@ -1,4 +1,8 @@
 <script setup lang="ts">
+import { onMounted } from 'vue'
+
+onMounted(()=>document.title = 'لیست آدرس ها')
+
 </script>
 
 <template>
--- src/views/NewAddressView.vue
+++ src/views/NewAddressView.vue
@@ -9,27 +9,27 @@
           <h4>لطفا مشخصات و آدرس خود را وارد کنید </h4>
         </div>
         <div class="card-body row d-flex align-items-center">
-          <form-input @send-data="data['name'] = $event" class="col col-lg-6 col-xl-4" id="name"
+          <form-input @send-data="data['name'] = $event" class="col-12 col-lg-6 col-xl-4" id="name"
                       title="نام" placeholder="مثال: عیسی" type="text" min-length="3"
                       :validation="validationError.name"
                       :require-input="false" error="نام باید حداقل دارای 3 کاراکتر باشد" />
-          <form-input @send-data="data['lastName'] = $event" class="col col-lg-6 col-xl-4"
+          <form-input @send-data="data['lastName'] = $event" class="col-12 col-lg-6 col-xl-4"
                       id="last-name"
                       title="نام خانوادگی" placeholder="مثال: رضایی" type="text" min-length="3"
                       :validation="validationError.lastName" :require-input="false"
                       error="نام خانوادگی باید حداقل دارای 3 کاراکتر باشد" />
-          <form-input @send-data="data['phoneNumber'] = $event" class="col col-lg-6 col-xl-4"
+          <form-input @send-data="data['phoneNumber'] = $event" class="col-12 col-lg-6 col-xl-4"
                       id="phone-number" title="شماره تلفن همراه" placeholder="مثال: 09121234567"
                       type="text" min-length="11" :validation="validationError.phoneNumber"
                       :require-input="false"
                       error="شماره وارد شده صحیح نمیباشد" />
-          <form-input @send-data="data['telephone'] = $event" class="col col-lg-6 col-xl-4"
+          <form-input @send-data="data['telephone'] = $event" class="col-12 col-lg-6 col-xl-4"
                       id="telephone"
                       title="شماره تلفن ثابت" placeholder="مثال: 0211234567" type="text"
                       min-length="11"
-                      :validation="validationError.telephone" :require-input="false"
+                      :validation="validationError.telephone" :require-input="true"
                       error="شماره وارد شده صحیح نمیباشد" />
-          <form-input @send-data="data['address'] = $event" class="col col-lg-12 col-xl-8"
+          <form-input @send-data="data['address'] = $event" class="col-12 col-lg-12 col-xl-8"
                       id="address"
                       title="آدرس" placeholder=" " type="text" min-length="10"
                       :validation="validationError.address"
@@ -44,8 +44,8 @@
 
 <script setup lang="ts">
 import FormInput from '@/components/form/input.vue'
-import { reactive, ref } from 'vue'
-
+import { onMounted, reactive, ref } from 'vue'
+onMounted(()=>document.title = 'ثبت آدرس')
 interface formData {
   name: string,
   lastName: string,
